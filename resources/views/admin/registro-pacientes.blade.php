4f5003361aeccf3f8ef01b4d80481aad9a3358dd
Hermayonick-Catalina-Hernandez-Gonzalez <2130036@upv.edu.mx>
2024-06-08 17:24:53 -0600

    diseño terminado

--- resources/views/admin/registro-pacientes.blade.php
+++ resources/views/admin/registro-pacientes.blade.php
@@ -5,69 +5,68 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Llene cuidadosamente los datos para registrar un nuevo paciente") }}
-                </div>
+    <div class="py-12 flex justify-center">
+        <div class="max-w-lg w-full sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <!-- Nombre -->
+                    <div>
+                        <x-input-label for="nombre" :value="__('Nombre(s)*')" />
+                        <x-text-input id="nombre" class="block mt-1 w-full" type="text" name="nombre"
+                            :value="old('nombre')" required autofocus autocomplete="nombre" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <!-- Apellidos -->
+                    <div class="mt-4">
+                        <x-input-label for="apellido" :value="__('Apellidos*')" />
+                        <x-text-input id="apellido" class="block mt-1 w-full" type="text" name="apellido"
+                            :value="old('apellidos')" required autocomplete="apellido" />
+                        <x-input-error :messages="$errors->get('apellidos')" class="mt-2" />
+                    </div>
+
+                    <!-- Fecha de nacimiento -->
+                    <div class="mt-4">
+                        <x-input-label for="fecha_nacimiento" :value="__('Fecha de nacimiento*')" />
+                        <x-text-input id="fecha_nacimiento" class="block mt-1 w-full" type="date"
+                            name="fecha_nacimiento" :value="old('fecha_nacimiento')" required autocomplete="fecha_nacimiento" />
+                        <x-input-error :messages="$errors->get('fecha_nacimiento')" class="mt-2" />
+                    </div>
+
+                    <!-- Teléfono -->
+                    <div class="mt-4">
+                        <x-input-label for="telefono" :value="__('Teléfono*')" />
+                        <x-text-input id="telefono" class="block mt-1 w-full" type="text" name="telefono"
+                            :value="old('telefono')" required autocomplete="telefono" />
+                        <x-input-error :messages="$errors->get('telefono')" class="mt-2" />
+                    </div>
+
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <x-input-label for="email" :value="__('Email*')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Rol -->
+                    <div class="mt-4">
+                        <x-input-label for="role" :value="__('Rol*')" />
+                        <select id="role" name="role"
+                            class="block mt-1 w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 bg-white text-gray-900">
+                            <option value="Paciente">{{ __('Paciente') }}</option>
+                        </select>
+                        <x-input-error :messages="$errors->get('role')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center justify-center mt-4">
+                        <x-primary-button class="ms-4">
+                            {{ __('Registrar') }}
+                        </x-primary-button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 
-    <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Nombre -->
-        <div>
-            <x-input-label for="nombre" :value="__('Nombre(s)*')" />
-            <x-text-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre')" required autofocus autocomplete="nombre" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Apellidos -->
-        <div class="mt-4">
-            <x-input-label for="apellido" :value="__('Apellidos*')" />
-            <x-text-input id="apellido" class="block mt-1 w-full" type="text" name="apellido" :value="old('apellidos')" required autocomplete="apellido" />
-            <x-input-error :messages="$errors->get('apellidos')" class="mt-2" />
-        </div>
-
-        <!-- Fecha de nacimiento -->
-        <div class="mt-4">
-            <x-input-label for="fecha_nacimiento" :value="__('Fecha de nacimiento*')" />
-            <x-text-input id="fecha_nacimiento" class="block mt-1 w-full" type="date" name="fecha_nacimiento" :value="old('fecha_nacimiento')" required autocomplete="fecha_nacimiento" />
-            <x-input-error :messages="$errors->get('fecha_nacimiento')" class="mt-2" />
-        </div>
-
-        <!-- Teléfono -->
-        <div class="mt-4">
-            <x-input-label for="telefono" :value="__('Teléfono*')" />
-            <x-text-input id="telefono" class="block mt-1 w-full" type="text" name="telefono" :value="old('telefono')" required autocomplete="telefono" />
-            <x-input-error :messages="$errors->get('telefono')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Rol -->
-        <div class="mt-4">
-            <x-input-label for="role" :value="__('Rol')" />
-            <select id="role" name="role" class="block mt-1 w-full form-select">
-                <option value="Paciente">{{__('Paciente')}}</option>
-            </select>
-            <x-input-error :messages="$errors->get('role')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-center mt-4"> 
-            <x-primary-button class="ms-4">
-                {{ __('Registrar') }}
-            </x-primary-button>
-        </div>
-    </form>
-    </x-guest-layout>
 </x-app-layout>
