a5317bd7f84b88e3bf558b06939292684522152c
Diego Ortiz <2030282@upv.edu.mx>
2024-08-14 20:06:13 -0600

    Ultimos cambios en consulta

--- app/Models/Consulta.php
+++ app/Models/Consulta.php
@@ -8,4 +8,44 @@ use Illuminate\Database\Eloquent\Model;
 class Consulta extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'paciente_id',
+        'medico_id',
+        'motivo_consulta',
+        'notas_padecimiento',
+        'edad',
+        'talla',
+        'temperatura',
+        'peso',
+        'frecuencia_cardiaca',
+        'alergias',
+        'diagnostico',
+        'solicitar_estudios',
+        'indicaciones_estudios',
+        'medicacion',
+        'cantidad',
+        'frecuencia',
+        'duracion',
+        'notas_receta',
+    ];
+
+    protected $casts = [
+        'solicitar_estudios' => 'array',
+        'indicaciones_estudios' => 'array',
+        'medicacion' => 'array',
+        'cantidad' => 'array',
+        'frecuencia' => 'array',
+        'duracion' => 'array',
+    ];
+
+    public function paciente()
+    {
+        return $this->belongsTo(User::class, 'paciente_id');
+    }
+
+    public function medico()
+    {
+        return $this->belongsTo(User::class, 'medico_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,7 @@ Route::middleware(['auth', 'MedicoMiddleware'])->group(function () {
     Route::get('registro-pacientes', [RegistroPacientesMEDICOController::class, 'index'])->name('registro-pacientes'); //* Vista para registrar pacientes
     Route::post('registro-pacientes', [RegistroPacientesMEDICOController::class, 'registro_paciente'])->name('registro-pacientes.store'); //* POST a registrar pacientes a BD
     Route::get('consultas', [ConsultasMEDICOController::class, 'index'])->name('consultas');
+    Route::post('consultas', [ConsultasMEDICOController::class, 'storeConsulta'])->name('consultas.storeConsulta'); //* POST a registrar consultas a BD
     Route::get('crear-cita', [CrearCitasMEDICOController::class, 'index'])->name('crear-cita');
     Route::post('crear-cita', [CrearCitasMEDICOController::class, 'store'])->name('crear-cita.store');
 });
